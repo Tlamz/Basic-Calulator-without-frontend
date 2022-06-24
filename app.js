ccc12fd97dd74b253d2ffee9a05fd2a86eca7dbf
Tosin Oyelami <tosin_oyelami@yahoo.com>
2022-06-24 11:53:19 +0100

    initial commit

---
+++ app.js
@@ -0,0 +1,24 @@
+document.write("<h1>Basic Calculator</h1>");
+
+const num1 = parseInt(prompt("Enter first number: "));
+const operator = prompt("Enter operator +, -, * or / : ");
+const num2 = parseInt(prompt("Enter second number: "));
+
+let result;
+if (isNaN(num1) || isNaN(num2)) {
+  alert("Error! Kindly restart");
+} else if (operator === "+") {
+  result = num1 + num2;
+} else if (operator === "-") {
+  result = num1 - num2;
+} else if (operator === "*") {
+  result = num1 * num2;
+} else if (operator === "/"){
+  result = num1 / num2;
+} else {
+  alert('Error! Kindly check the operator used');
+}
+
+let message = num1 + " " + operator + " " + num2 + " = " + result;
+
+alert(message);
